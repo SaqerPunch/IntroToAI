5e2f8f9797f23b3736d6796fe2fa58a3b098ee85
SaqerPunch <115329139+SaqerPunch@users.noreply.github.com>
2023-07-14 00:45:25 -0400

    Update priority queue.py

--- priority queue.py
+++ priority queue.py
@@ -1,24 +1,60 @@
 class Astar_Node:
-    def __init__(self, weight, heuristic,):
+    def __init__(self, weight, heuristic):
         self.weight = weight
         self.heuristic = heuristic 
+        self.priority = weight + heuristic
         self.next = None
 
-class SaqerPriorityQueue:
+class Astar_PriorityQueue:
     def __init__(self):
         self.front = None
 
-    def push(self, val):
-        temp = SaqerNode(val)
-
+    def isEmpty(self):
         if self.front == None:
-            self.front = temp
-            return
-
-        pnt = self.front
-        while temp.val < self.front.val:
-            pnt = self.front.next
+            return True
+        else:
+            return False
         
 
+    def push(self, weight, heuristic):
+        
+        temp = Astar_Node(weight,heuristic)
+        if self.isEmpty() == True:
+            self.front = temp
+            return
+        
+        else:
+            if temp.priority < self.front.priority:
+                temp.next = self.front
+                self.front = temp
             
+            else:
+
+                ptr = self.front
+                while ptr:
+
+                    if ptr.priority <= self.front.priority:
+                        break
+                    
+                    ptr = ptr.next
+                
+                temp.next = ptr.next
+                ptr.next = temp
+
+        return
+    
+    def pop(self):
+
+        if self.isEmpty():
+            return None
+        
+        else:
+            self.front = self.front.next
+            return self.front
+
+    def priority(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.front.priority
 
