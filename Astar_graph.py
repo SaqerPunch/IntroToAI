d290ad4fae9723f6eefe0ed7dd911d41612993aa
SaqerPunch <115329139+SaqerPunch@users.noreply.github.com>
2023-07-15 19:51:32 -0400

    Update Astar_graph.py

--- Astar_graph.py
+++ Astar_graph.py
@@ -1,27 +1,12 @@
 from AStar_PriorityQueue import Astar_Node
-
-class Astar_graph:
-    def __init__(self, num):
+class Astargraph:
+    def init(self, num):
         self.x = num
-        self.graph = [None] * self.x
+        self.graph = [[] for i in range(self.x)]
 
-    def edge_add(self, x, y,weight):
+    def edge_add(self, x, y, weight):
         node = Astar_Node(index=x)
-        node.next = self.graph[y]
-        self.graph[y].append((node,weight))
+        self.graph[y].append((node, weight))
 
         node = Astar_Node(index=y)
-        node.next = self.graph[x]
-        self.graph[x].append((node,weight))
-
-
-"""
-class Astar_graph:
-    def __init__(self, num_nodes):
-        self.num_nodes = num_nodes
-        self.adj_list = {node: set() for node in range(self.num_nodes)}
-
-    def edge_add(self, first_node, second_node, weight):
-        self.adj_list[second_node].add((first_node,weight))
-"""
-
+        self.graph[x].append((node, weight))
